ec0e818b3599970d9a5fce4bd342bac47a2b3eba
STAG141\Matthew <msvanhoose@eagles.usi.edu>
2021-12-06 11:56:40 -0500

    Added 2 Stateless Widgets. 1: Main widget called by runApp. 2: Temp PageOne  widget. Returns Scaffold, print 'Hello World' to center

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LearningApp());
+}
+
+class LearningApp extends StatelessWidget {
+  const LearningApp ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PageOne(),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  const PageOne ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
 }
